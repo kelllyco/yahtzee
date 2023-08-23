82b338c7c96972137f64ff0099faf29f1ccd9e2e
kellycochran <kac1011@icloud.com>
2023-08-23 17:47:44 -0400

    updates before leaving laptop

--- score.h
+++ score.h
@@ -2,7 +2,7 @@
 #define SCORE
 
 #include <iostream>
-#include "diceCup.h"
+#include "Dice.h"
 #include "score.h"
 
 using namespace std;
@@ -16,6 +16,9 @@ class Score
 
       // bool to indicate if a score has been used, impt if user has to put 0 in a category
       bool hasValue = false;
+
+      // private member var to hold passed in array of dice
+      Dice* arrayOfDice;
 
    public:
       // point val getter and setter
@@ -31,68 +34,93 @@ class Score
 
 };
 
+// the following are concrete classes for the score class
+// they hold a calculate value function which will be different for each class
+// they also have their typename, which will never change
 class Aces : public Score {
    public:
+      Aces(*Dice);
+
       int calculateValue() override;
       const string TYPENAME = "aces";
 };
 
 class Twos : public Score {
    public:
+      Twos(*Dice);
+
       int calculateValue() override;
       const string TYPENAME = "twos";
 };
 
 class Threes : public Score {
    public:
+      Threes(*Dice);
+
       int calculateValue() override;
       const string TYPENAME = "threes";
 };
 
 class Fours : public Score {
    public:
+      Fours(*Dice);
+
       int calculateValue() override;
       const string TYPENAME = "fours";
 };
 
 class Fives : public Score {
    public:
+      Fives(*Dice);
+
       int calculateValue() override;
       const string TYPENAME = "fives";
 };
 
 class Sixes : public Score {
    public:
+      Sixes(*Dice);
+
       int calculateValue() override;
       const string TYPENAME = "sixes";
 };
 
 class ThreeOfAKind : public Score {
    public:
+      ThreeOfAKind(*Dice);
+
       int calculateValue() override;
       const string TYPENAME = "three of a kind";
 };
 
 class FourOfAKind : public Score {
    public:
+      FourOfAKind(*Dice);
+
       int calculateValue() override;
       const string TYPENAME = "four of a kind";
 };
 
 class FullHouse : public Score {
    public:
+      FullHouse(*Dice);
+
       int calculateValue() override;
       const string TYPENAME = "full house";
 };
 
 class Yahtzee : public Score {
    public:
+      Yahtzee(*Dice);
+
       int calculateValue() override;
       const string TYPENAME = "yahtzee";
 };
 
 class Chance : public Score {
    public:
+      Chance(*Dice);
+      
       int calculateValue() override;
       const string TYPENAME = "chance";
 };

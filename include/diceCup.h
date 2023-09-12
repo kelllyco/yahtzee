cf53722a5a75a4ddbd3e2e250830bbbcaeed2aef
kellycochran <kac1011@icloud.com>
2023-09-11 21:16:00 -0400

    cleaned and commented

--- include/diceCup.h
+++ include/diceCup.h
@@ -1,16 +1,13 @@
 // this file contains the function, constructor, and variable definitions for the cup of dice class
 // it can be included in other files to allow the cup of dice class to be utilized inside them
 
+// the dicecup class has ownership over the dice objects, and it handles all interactions that manage
+// multiple dice
+
 #ifndef DICE_CUP
 #define DICE_CUP
 
-// should probably make a different program here, a yahtzee cheater program, that takes given die and sums stuff up, bc that function will be used lots and is very important
-// want to figure out a fun ui for this too, do research once you have wifi
-// honestly will want to run this each time for when its the computer's turn
-
-// imports the user defined header files
 #include "Dice.h"
-
 #include <vector>
 
 using namespace std;
@@ -18,31 +15,41 @@ using namespace std;
 class DiceCup
 {
    public:
-      // default constructor
+      // inits five dice and add them to the array
+      // init does not roll, have to do the first actual roll as you would in the game irl
       DiceCup();
 
+      // this function adds the dice stored at index "int" to the vector storing the dice to be rolled
       void addToCup(int);
+
+      // rolls each indv dice in the cup vector, also removes them from the vector, leaving w/ an empty cup
       void roll();
 
+      // sets times rolled to zero, is called at the end of each round
       void resetTimesRolled();
 
+      // this returns a pointer to the cup of dice vector, its used because scorecard needs to be able to refrence
+      // it, as does front end stuff in game, would like to rework later to keep this pointer safer
       Dice* dangerReturnsDiceArrayPtr();
 
 
    private:
       // this is the vector that will only be updated by addToCup, and it will be wiped at the end of
       // roll, it will store the indexes of the dice to be rolled within the dice array
-      vector <int> diceIndexesVector;
+      // a pointer to it does get passed to scorecard and game as mentioned earlier, would like to
+      // improve this system in the future
+      vector <int> cupOfDice;
 
-      // so this is the array where the five dice are stored, within which the vector will point
+      // this is the array where the five dice are stored, within which the vector points
       Dice diceArray[5];
 
-      // function to only ever really be called within roll function
+      // function to only ever be called within roll function, pops dice off the vector
       int removeFromCup();
 
-      // track times rolled, obvi can't go over three
+      // track times rolled, obvi can't go over three per game rules
       int timesRolled;
 
+      // makes the game class a friend so front end stuff can stay seperate
       friend class Game;
 
 
--- src/diceCup.cpp
+++ src/diceCup.cpp
@@ -1,12 +1,8 @@
 #include "/Users/kellycochran/Desktop/yahtzee/include/diceCup.h"
 #include "/Users/kellycochran/Desktop/yahtzee/include/Dice.h"
 
-#include <iostream>
-
 using namespace std;
 
-// default declaration should init five dice and add them to the array
-// init does not roll, have to do the first actual roll as you would in the game irl
 DiceCup::DiceCup()
 {
    timesRolled = 0;
@@ -24,70 +20,32 @@ Dice* DiceCup::dangerReturnsDiceArrayPtr()
    return diceArray;
 }
 
-// adds the index to the vector, while checking to make sure dice cup is not overfilled
+
 void DiceCup::addToCup(int diceIndex)
 {
-   bool duplicate = false;
-
-   for (int i = 0; i < diceIndexesVector.size(); i++)
-   {
-      if (diceIndexesVector[i] == diceIndex)
-      {
-         duplicate = true;
-      }
-   }
-
-   // tbh this is probably overkill bc some of these instances cant happen due to other ones
-   // but better safe than sorry
-   if (duplicate)
-   {
-      cout<<"Cannot add a dice that's already been added to the cup!"<<endl;
-
-   }
-   else if ((diceIndex < 0) || (diceIndex > 4))
-   {
-      cout<<"Cannot add the "<<diceIndex<<" number dice, there are only 5 in the game!"<<endl;
-   }
-   else if (diceIndexesVector.size() > 4)
-   {
-      cout<<"Cannot add more dice to cup, there are only 5 in the game!"<<endl;
-   }
-   else
-   {
-      diceIndexesVector.push_back(diceIndex);
-   }
-   
+   cupOfDice.push_back(diceIndex);
 }
 
-// returns the held index at the top of the vector and pops off the element (if thinking of as a stack)
+
 int DiceCup::removeFromCup()
 {
-   int heldIndex = -1;
-
-   // if its empty, alert and return -1
-   if (diceIndexesVector.empty())
-   {
-      cout<<"Cannot remove dice from an empty cup!"<<endl;
-      return heldIndex;
-   }
+   int heldIndex;
 
    // get the index held at the back of the vector
-   heldIndex = diceIndexesVector[diceIndexesVector.size() - 1];
+   heldIndex = cupOfDice[cupOfDice.size() - 1];
 
    // get rid of the end of the vector
-   diceIndexesVector.pop_back();
+   cupOfDice.pop_back();
 
    return heldIndex;
 
 }
 
-// rolls the dice in the cup, also removes them and all so that's handled
-// should leave the user with an empty cup (aka an empty diceIndexesVector)
 void DiceCup::roll()
 {
    if (timesRolled < 3)
    {
-      int amntToRoll = diceIndexesVector.size();
+      int amntToRoll = cupOfDice.size();
 
       // runs loop for number of dice needed to roll
       for (int i = 0; i < amntToRoll; i++)
@@ -96,10 +54,6 @@ void DiceCup::roll()
       }
 
       timesRolled += 1;
-   }
-   else
-   {
-      cout<<"Rolled too many times, you cant roll more than three times!"<<endl;
    }
 }
 

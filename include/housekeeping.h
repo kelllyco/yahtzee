bbc7841bfb8d59db21b33435f84240838f68eb1d
kellycochran <kac1011@icloud.com>
2023-09-11 23:50:58 -0400

    cleaned and commented

--- include/housekeeping.h
+++ include/housekeeping.h
@@ -1,18 +1,15 @@
-// this file contains the function, constructor, and variable definitions for the housekeeping class
-// it can be included in other files to allow the housekeeping class to be utilized inside of them
+// this class literally just sets the seed for the random function, its a seperate class because i wanted to
+// be certain that i wouldn't accidentally call it twice, and this seemed like the safest solution
 
 #ifndef HOUSEKEEPING_H
 #define HOUSEKEEPING_H
 
-// okay so i recognize that the use of static here is probably very undesireable, but i needed a way to essentially fake a global variable
-// reasons for this:
-// 1. global variables are bad
-// 2. i only wanted seed to be called once, and i reallly didnt want to do it at the beginning of my driver and pass it into each lower function that i call
-// 3. if it wasn't static, i could end up with seed getting called each time a new housekeeping variable is made
-// 3. because i would like to avoid this, the variable needs to be shared across the entire class
-// 3. you might ask, why just not create a new housekeeping object multiple times?
-// 3. my answer is basically that i'm still in the early stages of creating this program and i'm not certain which parts are going to be repeated or not yet
-// 3. i'd rather create a backdoor solution now and maybe come back to it, but one that will nevertheless work, instead of just plopping something in that could mess me up later
+#include <ctime>
+#include <iostream>
+#include <cstdlib>
+#include <stdexcept>
+
+using namespace std;
 
 class Housekeeping
 {
@@ -20,8 +17,6 @@ class Housekeeping
       // function to be called once, will use ctime to set the seed for rolling randomization
       static void setSeed();
 
-   private:
-      static int timesSeedSet;
 };
 
 
--- src/housekeeping.cpp
+++ src/housekeeping.cpp
@@ -1,26 +1,6 @@
 #include "/Users/kellycochran/Desktop/yahtzee/include/housekeeping.h"
-#include <ctime>
-#include <iostream>
-#include <cstdlib>
-
-using namespace std;
-
-// set the static values to 0
-int Housekeeping::timesSeedSet = 0;
 
 void Housekeeping::setSeed()
 {
-   // if this has been called before throw an error
-   if (timesSeedSet > 0)
-   {
-      // could probably throw a user defined error here hmmm
-      cout<<"DONT RESEED THE RANDOMIZATION"<<endl;
-   }
-
-   // otherwise do not throw the error, set the time as needed and iterate timesSeedSet
-   else 
-   {
-      srand(time(0));
-      timesSeedSet += 1;
-   }  
+   srand(time(0));
 }
